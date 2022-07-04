27a6a1b0d199a8b7869e2524881c720244f80365
Ifo Ikede <ifoitech@gmail.com>
2022-07-04 04:47:59 -0300

    add prompt for size of grid

--- app.js
+++ app.js
@@ -7,26 +7,58 @@ container.className = 'container';
 const changeBtn = document.getElementById("changeBtn") ;
 console.log(changeBtn) ;
 
-
 let numberOfBoxes = 16 * 16 ;
 
+changeBtn.addEventListener("click", changeNumOfBoxes) ;
+
+// function createGrid() {
     // create boxes
-for( let i= 1; i <= numberOfBoxes; i++ ) {
-    const box = document.createElement('div');
-     let id = i ;
-    box.className = 'box';
-    box.setAttribute("id", `box${id}`)  ;
+    for( let i= 1; i <= numberOfBoxes; i++ ) {
+        const box = document.createElement('div');
+        let id = i ;
+        box.className = 'box';
+        box.setAttribute("id", `box${id}`)  ;
 
-    box.addEventListener("mouseover", handleMouseOverBox);
-              
-      // add box to container
-    container.appendChild(box);
-}
+        box.addEventListener("mouseover", handleMouseOverBox);
+                
+        // add box to container
+        container.appendChild(box);
 
+         }
+        
+    body.appendChild(container); 
 
-body.appendChild(container); 
+// }
+
+// createGrid() ;
+
 
 function handleMouseOverBox () {
    this.style.backgroundColor = "red" ;
+
+}
+
+function changeNumOfBoxes () {
+    let input = prompt("Please enter the number squares per side you want: max 100") ;
+       if (isValidInput) {
+            let size = parseInt(input) ;
+            numberOfBoxes = size * size ;  
+            createGrid() ;
+
+        } else alert("Please enter a valid number less between 1 and 100") ;
+}
+
+function isValidInput(userInput) {
+    If( userInput === undefined || userInput === null || !Number.isInteger(userInput)) 
+    return false ;
+          
+    let num = parsInt(userInput) ;
+    if(  1 <= num  && num  <= 100) {
+
+    return true ;
+        }else  {
+            return false ;
+        }
+    
 
 }
